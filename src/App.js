e64ac4e45147b555fc5273b1caf3fb00a57465e6
suwat <suwat.nakchukaew@gmail.com>
2017-08-27 21:44:45 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    noets: ["note"]
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.noets.length > 0 &&<p>You have notes </p>}
       </div>
     );
   }
