f3b89da404d029557cd3ba290a307adf2fc4fe7a
SUWAT <suwat@thinknet.co.th>
2017-08-30 14:51:49 +0700

    setstate

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    notes: [
+      "note@1",
+      "note@2",
+      "note@3",
+      "note@4",
+    ]
+  }
   render() {
     return(
-      <input type="text" onChange={(e) => { console.log(e.target.value) }} />
+      <div>
+        <ul>
+          {
+            this.state.notes.map(
+              (note, index) => <li key={index}>{note}</li>
+            )
+          }
+        </ul>
+        <input type="text" onKeyUp={(e) => e.keyCode===13 && this.setState({notes: [...this.state.notes, e.target.value]})}/>
+      </div>
     )
   }
 }
