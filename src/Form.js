35bd6ea356f4c5802ffcd0c529f6a7b3b4741321
SUWAT <suwat@thinknet.co.th>
2017-08-30 17:14:28 +0700

    ทำ controller component  controller input

--- src/Form.js
+++ src/Form.js
@@ -1,16 +1,42 @@
 import React, { Component } from 'react'
 
 class FormComponet extends Component {
+  state = {
+    name: '',
+    address: ''
+  }
+
+  createContact = (e) => {
+    e.preventDefault()
+    console.log(this.state)
+
+  }
+  changeState = state => (e) => (
+    this.setState({ [state]: e.target.value })
+  )
+
   render() {
-    return(
+    const { name, address } = this.state
+    return (
       <div>
         <h2>Address From</h2>
         <form>
           <div>
-            <input type="text" id="name" />
-            <input type="text" id="address" />
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={this.changeState('name')} />
+            <input
+              type="text"
+              id="address"
+              value={address}
+              onChange={this.changeState('address')} />
           </div>
-          <button type="submit">create</button>
+          <button
+            type="submit"
+            onClick={this.createContact}
+          >create</button>
         </form>
       </div>
     )
