42657a2d7c7f1a0a4a3b851319dba1150baed6ab
SUWAT <suwat@thinknet.co.th>
2017-08-30 14:42:34 +0700

    event

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
-  state = {
-    noets: ["note"]
-  }
   render() {
-    return (
-      <div className="App">
-        {this.state.noets.length > 0 &&<p>You have notes </p>}
-      </div>
-    );
+    return(
+      <input type="text" onChange={(e) => { console.log(e.target.value) }} />
+    )
   }
 }
 
