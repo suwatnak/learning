91e01eb3ea3fe62ed7ad143dccb0b7a91ea6f5e4
SUWAT <suwat@thinknet.co.th>
2017-09-07 13:52:30 +0700

    location

--- client/src/containers/Articles.js
+++ client/src/containers/Articles.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-
+import className from 'classnames'
 class ArticlesContainer extends Component {
   state = {
-    articles: []
+    articles: [],
+    pagination: '',
   }
 
   componentDidMount() {
@@ -11,33 +12,60 @@ class ArticlesContainer extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    const {match} = this.props
-    const { match: prevMatch} = prevProps
+    const { match, location } = this.props
+    const { match: prevMatch, location: prevLocation } = prevProps
 
-    if(match.params.categoryId !== prevMatch.params.categoryId) {
+    if (
+      (match.params.categoryId !== prevMatch.params.categoryId) ||
+      (location.search !== prevLocation.search)) {
       this.loadArticles()
     }
   }
+
   loadArticles() {
-    const { categoryId } = this.props.match.params
-    fetch(`http://localhost:8000/articles?categoryId=${categoryId}`)
+    const { location: { search }, match: { params } } = this.props
+    const { categoryId } = params
+    const page = new URLSearchParams(search).get('page')
+    fetch(`http://localhost:8000/articles?categoryId=${categoryId}&page=${page || 1}`)
       .then(res => res.json())
-      .then(({ articles }) => this.setState({ articles }))
+      .then(({ articles, meta }) => this.setState({ articles, pagination: meta }))
   }
   render() {
-    const { articles } = this.state
-    console.log(articles)
+    const { articles, pagination: { page, totalPages } } = this.state
+    const { categoryId } = this.props.match.params
+
     return (
-      <ul>
-        {
-          articles.map(({ id, title }) =>
-            <Link
-            key={id}
-            to={`/articles/${id}`}
-            className='nav-link'>{title}</Link>
-          )
-        }
-      </ul>
+      <div>
+        <ul>
+          {
+            articles.map(({ id, title }) =>
+              <Link
+                key={id}
+                to={`/articles/${id}`}
+                className='nav-link'>{title}</Link>
+            )
+          }
+        </ul>
+        <nav>
+          <ul className='pagination'>
+            {
+              Array.from({ length: totalPages }).map((_, index) => {
+                const currentIndex = index + 1
+
+                return (
+                  <li
+                    key={currentIndex}
+                    className={className(['page-item', { active: currentIndex === +page }])}>
+                    <Link
+                      to={`/categories/${categoryId}/articles?page=${currentIndex}`}
+                      className='page-link'>{currentIndex}</Link>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+      </div>
     )
   }
 }
